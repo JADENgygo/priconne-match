f2f5329c5ad4b0c365c62e6790c81519529e6faa
JADEN <29948746+JADENgygo@users.noreply.github.com>
2022-09-14 10:49:25 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -81,10 +81,10 @@ function MyApp({ Component, pageProps }: AppProps) {
             <i className="bi bi-twitter"></i>
           </a>
           <a href="https://priconne-portfolio.vercel.app" className="link-dark" >
-            プリコネツール
+            闇プリン開発室
           </a>
         </div>
-        <div>画像: &copy; Cygames, Inc.</div>
+        <div>一部画像 &copy; Cygames, Inc.</div>
       </footer>
     </div>
   );
